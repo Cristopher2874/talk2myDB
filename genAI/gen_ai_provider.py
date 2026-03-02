5c77731b94488db7439bedfa200bcb33c5012b9c
Cristopher2874 <cris.main27@gmail.com>
2026-03-02 17:22:59 -0600

    Project set up

---
+++ genAI/gen_ai_provider.py
@@ -0,0 +1,39 @@
+import os
+from typing import Any
+from langchain_oci import ChatOCIGenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class GenAIProvider:
+    """Singleton provider for OCI GenAI LLM clients."""
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        pass
+
+    def build_oci_client(self, model_id:str="xai.grok-4-fast-non-reasoning", model_kwargs:dict[str,Any] = {}):
+        client = ChatOCIGenAI(
+            model_id=model_id,
+            service_endpoint=os.getenv("SERVICE_ENDPOINT"),
+            compartment_id=os.getenv("COMPARTMENT_ID"),
+            model_kwargs=model_kwargs,
+            auth_profile="API-USER",
+        )
+
+        return client
+    
+    def update_oci_client(
+        self, 
+        client:ChatOCIGenAI, 
+        model_id:str="xai.grok-4-fast-non-reasoning", 
+        model_kwargs:dict[str,Any] = {}
+    ):
+        client.model_id=model_id
+        client.model_kwargs=model_kwargs
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from talk2mydb!")
+
+
+if __name__ == "__main__":
+    main()
